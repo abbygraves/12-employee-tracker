a43555e8bfa592171b86f84e911308d214ce94ee
abbygraves <abbygraves14@gmail.com>
2022-08-02 06:56:43 -0500

    began writing inquirer prompt questions

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 //  WORK IN PROGRESS
+
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const db = require('./db/connection');
+const mysql = require('mysql2');
+
+
+// MAIN MENU
+const displayMenu = () => {
+  console.log(`
+  ███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗
+  ██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝
+  █████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗  
+  ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝  
+  ███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗
+  ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝
+                                                                      
+       ████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗         
+       ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗        
+          ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝        
+          ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗        
+          ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║        
+          ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                                      
+  `)
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'mainMenu',
+      message: 'Welcome to Employee Tracker. What would you like to do?',
+      choices: 
+        [
+          'View All Departments',
+          'View All Roles',
+          'View All Employees',
+          'Add a Department',
+          'Add a Role',
+          'Add an Employee',
+          'Update an Employee Role'
+        ]
+    }
+  ])
+}
+
+displayMenu();
