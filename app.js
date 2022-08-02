72372a4dea570b4b092458c803c9769c26f77d0b
abbygraves <abbygraves14@gmail.com>
2022-08-02 07:34:59 -0500

    inquirer main menu

--- app.js
+++ app.js
@@ -28,18 +28,47 @@ const displayMenu = () => {
       type: 'list',
       name: 'mainMenu',
       message: 'Welcome to Employee Tracker. What would you like to do?',
-      choices: 
-        [
-          'View All Departments',
-          'View All Roles',
-          'View All Employees',
-          'Add a Department',
-          'Add a Role',
-          'Add an Employee',
-          'Update an Employee Role'
-        ]
+      choices: [
+        'View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add a Department',
+        'Add a Role',
+        'Add an Employee',
+        'Update an Employee Role',
+        new inquirer.Separator(),
+        'Exit Application',
+        new inquirer.Separator(),
+      ]
     }
-  ])
+  ]).then(menuResponse => {
+    switch (menuResponse.menu) {
+      case 'View All Departments':
+        viewDepartments();
+        break;
+      case 'View All Roles':
+        viewRoles();
+        break;
+      case 'View All Employees':
+        viewEmployees();
+        break;
+      case 'Add a Department':
+        addDepartment();
+        break;
+      case 'Add a Role':
+        addRole();
+        break;
+      case 'Add an Employee':
+        addEmployee();
+        break;
+      case 'Update an Employee Role':
+        updateEmployee();
+        break;
+      case 'Exit Application':
+        default:
+        process.exit();
+    }
+  })
 }
 
 displayMenu();
